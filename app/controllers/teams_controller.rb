02ec5d73da435401446fd622576261c3e7995aeb
draftamerica <steelax42@gmail.com>
2017-08-23 13:09:16 -0400

    setting up heroku

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,7 @@
 class TeamsController < ApplicationController
   before_action :set_team, only: [:show, :edit, :update, :destroy]
 
-  include NFL
+  # include NFL
 
   def homepage
       puts "\n ****homepage*****"
